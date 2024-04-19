1bbf77a783557e5d922b553fe89ad9e811f049d5
André Roaldseth <andre.roaldseth@nav.no>
2024-04-19 15:39:16 +0200

    Ikke sett brukernavn og passord, det er i URL

--- app/src/main/kotlin/org/example/PostgresDataSourceBuilder.kt
+++ app/src/main/kotlin/org/example/PostgresDataSourceBuilder.kt
@@ -4,19 +4,17 @@ import com.zaxxer.hikari.HikariDataSource
 import java.lang.System.getProperty
 import java.lang.System.getenv
 
-// Understands how to create a data source from environment variables
 internal object PostgresDataSourceBuilder {
-    private const val DB_USERNAME_KEY = "DB_USERNAME"
-    private const val DB_PASSWORD_KEY = "DB_PASSWORD"
     private const val DB_URL_KEY = "DB_JDBC_URL"
 
     private fun getOrThrow(key: String): String = getenv(key) ?: getProperty(key)
 
     val dataSource by lazy {
         HikariDataSource().apply {
-            jdbcUrl = getOrThrow(DB_URL_KEY)
-            username = getOrThrow(DB_USERNAME_KEY)
-            password = getOrThrow(DB_PASSWORD_KEY)
+            jdbcUrl =
+                getOrThrow(DB_URL_KEY).also {
+                    println("DB_JDBC_URL: $it")
+                }
             maximumPoolSize = 10
             minimumIdle = 1
             idleTimeout = 10001
